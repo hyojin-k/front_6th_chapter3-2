897adcb234224c5b4daecfa574fb72879ac95bc9
HYOJIN <hyo.khj@gmail.com>
2025-08-27 01:51:58 +0900

    test: GREEN - 윤년 29일에 매년 반복을 선택하면 29일에만 생성한다

--- src/utils/repeatEventUtils.ts
+++ src/utils/repeatEventUtils.ts
@@ -41,6 +41,12 @@ function isValidRepeatDate(originalDate: Date, newDate: Date, repeatType: Repeat
     return originalDate.getDate() === newDate.getDate();
   }
 
+  if (repeatType === 'yearly') {
+    return (
+      originalDate.getMonth() === newDate.getMonth() && originalDate.getDate() === newDate.getDate()
+    );
+  }
+
   return true;
 }
 
@@ -54,20 +60,43 @@ export function generateRepeatEvents(event: Event, endDate: Date): Event[] {
 
   const events: Event[] = [];
   const startDate = new Date(event.date);
-  let currentDate = new Date(startDate);
 
   const repeatEndDate = new Date(event.repeat.endDate ?? '');
   const finalEndDate = repeatEndDate && repeatEndDate < endDate ? repeatEndDate : endDate;
 
-  while (currentDate <= finalEndDate) {
-    if (isValidRepeatDate(startDate, currentDate, event.repeat.type)) {
-      events.push({
-        ...event,
-        date: formatDateToString(currentDate),
-      });
-    }
+  // 윤년 처리
+  if (event.repeat.type === 'yearly') {
+    let currentYear = startDate.getFullYear();
+    const originalMonth = startDate.getMonth();
+    const originalDay = startDate.getDate();
 
-    currentDate = getNextRepeatDate(currentDate, event.repeat.type, event.repeat.interval);
+    while (currentYear <= finalEndDate.getFullYear()) {
+      const candidateDate = new Date(currentYear, originalMonth, originalDay);
+
+      if (candidateDate > finalEndDate) break;
+
+      if (candidateDate.getMonth() === originalMonth && candidateDate.getDate() === originalDay) {
+        events.push({
+          ...event,
+          date: formatDateToString(candidateDate),
+        });
+      }
+
+      currentYear += event.repeat.interval;
+    }
+  } else {
+    let currentDate = new Date(startDate);
+
+    while (currentDate <= finalEndDate) {
+      if (isValidRepeatDate(startDate, currentDate, event.repeat.type)) {
+        events.push({
+          ...event,
+          date: formatDateToString(currentDate),
+        });
+      }
+
+      currentDate = getNextRepeatDate(currentDate, event.repeat.type, event.repeat.interval);
+    }
   }
 
   return events;
