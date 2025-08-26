07fc0edfda8f3efdde813cef5c5de3f08f43e1f5
HYOJIN <hyo.khj@gmail.com>
2025-08-27 00:23:09 +0900

    test: GREEN - 매일 반복 일정을 생성한다

--- src/utils/repeatEventUtils.ts
+++ src/utils/repeatEventUtils.ts
@@ -1,5 +1,51 @@
-import { Event } from '../types';
+import { Event, RepeatType } from '../types';
 
-export function getRepeatEvents(events: Event[]) {
-  return;
+function formatDateToString(date: Date): string {
+  return date.toISOString().split('T')[0];
+}
+
+function getNextRepeatDate(currentDate: Date, repeatType: RepeatType, interval: number): Date {
+  const nextDate = new Date(currentDate);
+
+  switch (repeatType) {
+    case 'daily':
+      nextDate.setDate(nextDate.getDate() + interval);
+      break;
+
+    default:
+      break;
+  }
+
+  return nextDate;
+}
+
+/**
+ * 반복 일정 생성
+ */
+export function generateRepeatEvents(event: Event, endDate: Date): Event[] {
+  if (event.repeat.type === 'none') {
+    return [event];
+  }
+
+  const events: Event[] = [];
+  const startDate = new Date(event.date);
+  let currentDate = new Date(startDate);
+
+  while (currentDate <= endDate) {
+    if (endDate && currentDate > endDate) {
+      break;
+    }
+
+    if (currentDate >= startDate) {
+      const newEvent: Event = {
+        ...event,
+        date: formatDateToString(currentDate),
+      };
+      events.push(newEvent);
+    }
+
+    currentDate = getNextRepeatDate(currentDate, event.repeat.type, event.repeat.interval);
+  }
+
+  return events;
 }
